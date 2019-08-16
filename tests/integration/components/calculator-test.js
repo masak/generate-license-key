89fe928bd388ffb20bb31f9cff74b2aef9dd8864
Carl Mäsak <cmasak@gmail.com>
2019-08-16 15:42:37 +0200

    Fix up a failing/stale test

--- tests/integration/components/calculator-test.js
+++ tests/integration/components/calculator-test.js
@@ -7,20 +7,8 @@ module('Integration | Component | calculator', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<Calculator />`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <Calculator>
-        template block text
-      </Calculator>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.notEqual(this.element.textContent.trim(), '');
   });
 });
